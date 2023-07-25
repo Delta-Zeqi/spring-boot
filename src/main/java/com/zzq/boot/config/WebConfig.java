ab6ed61f6d51ee5310e0972f68333d7dc425f70f
root <1370357561@qq.com>
2023-07-25 11:40:20 +0800

    laptop first commit

---
+++ src/main/java/com/zzq/boot/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.zzq.boot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+/**
+ * @author 张泽奇
+ * @version 1.0
+ */
+@Configuration(proxyBeanMethods = false)
+public class WebConfig {
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        HiddenHttpMethodFilter hiddenHttpMethodFilter = new HiddenHttpMethodFilter();
+        hiddenHttpMethodFilter.setMethodParam("_m");
+        return hiddenHttpMethodFilter;
+    }
+
+}
---
+++ src/main/java/com/zzq/boot/controller/HelloController.java
@@ -0,0 +1,44 @@
+package com.zzq.boot.controller;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 张泽奇
+ * @version 1.0
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/starsky.jpg")
+    public String hello() {
+        return "aaa";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String getUser(){
+        return "GET-张三";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    public String saveUser(){
+        return "POST-张三";
+    }
+
+
+    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public String putUser(){
+        return "PUT-张三";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String deleteUser(){
+        return "DELETE-张三";
+    }
+
+}
