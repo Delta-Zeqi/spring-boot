45121a237760d1191a5ad53c836370abbb84b6ff
root <1370357561@qq.com>
2023-08-25 18:11:00 +0800

    @MatrixVariable 学习 1.自定义UrlPathHelper 2.add ParameterTestController.carsSell 3.add ParameterTestController.getBoss 4.在index中添加相关注意知识

--- src/main/java/com/zzq/boot/config/WebConfig.java
+++ src/main/java/com/zzq/boot/config/WebConfig.java
@@ -3,13 +3,16 @@ package com.zzq.boot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
 
 /**
  * @author 张泽奇
  * @version 1.0
  */
 @Configuration(proxyBeanMethods = false)
-public class WebConfig {
+public class WebConfig /*implements WebMvcConfigurer*/ {
 
     //扩展点：将_method 这个属性名改成自己想要的
     @Bean
@@ -19,4 +22,39 @@ public class WebConfig {
         return hiddenHttpMethodFilter;
     }
 
+    /**
+     * 对于使用矩阵变量，要把 UrlPathHelper 的 removeSemicolonContent 设置为 false
+     * 但是 SpringBoot 中没有与之对应的配置
+     * 所以需要我们自己自定义规则，根据 SpringBoot 官方文档的提示
+     * 不用@EnableWebMvc注解。使用 @Configuration + WebMvcConfigurer 自定义规则
+     * 那么就衍生出了 两种方式解决这个问题
+     * 1. 直接让 这个配置类 实现 WebMvcConfigurer 接口，重写 configurePathMatch 方法
+     * 2. 使用 @Bean 将重写了 configurePathMatch 方法的 WebMvcConfigurer 类型的实例 返回，将其注册到 Spring IOC 容器中
+     *
+     * 为什么 我们可以只实现接口中 的一个方法就行？
+     * 复习javase 基础知识：java8 中接口可以写 默认方法 了，所以不用在实现的时候，必须将所有的方法实现了
+     */
+
+    //这个就是第一种方式：配置类 实现 WebMvcConfigurer, 重写 configurePathMatch 方法
+    /*@Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        //不移除 ; 后面的内容。矩阵变量功能就可以生效了
+        urlPathHelper.setRemoveSemicolonContent(false);
+        //将 url路径小帮手 放入配置中
+        configurer.setUrlPathHelper(urlPathHelper);
+    }*/
+
+    //第二种方式：使用 @Baen 将 重写之后的 WebMvcConfigurer 注册到 容器中
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void configurePathMatch(PathMatchConfigurer configurer) {
+                UrlPathHelper urlPathHelper = new UrlPathHelper();
+                urlPathHelper.setRemoveSemicolonContent(false);
+                configurer.setUrlPathHelper(urlPathHelper);
+            }
+        };
+    }
 }
--- src/main/java/com/zzq/boot/controller/ParameterTestController.java
+++ src/main/java/com/zzq/boot/controller/ParameterTestController.java
@@ -1,7 +1,10 @@
 package com.zzq.boot.controller;
 
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.util.UrlPathHelper;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -62,4 +65,47 @@ public class ParameterTestController {
         map.put("content", content);
         return map;
     }
+
+    //1.语法： href="/cars/sell;low=34;brand=byd,audi,yd"
+    //2.SpringBoot默认是禁用了矩阵变量的功能
+    //      手动开启: 原理。
+    //      对于路径的处理。
+    //      使用 WebMvcAutoConfiguration --->
+    //      configurePathMatch(PathMatchConfigurer configurer) --->UrlPathHelper进行解析
+    //      removeSemicolonContent（移除分号内容），设置为 false 让其支持矩阵变量的。
+    // 所以需要我们自定义
+    //3.矩阵变量必须有url路径变量才能被解析
+    //@GetMapping("/cars/sell") //这样写，在自定义了 UrlPathHelper 之后，也是不起作用的，会报错”找不到路径“
+    //原因：矩阵变量是绑定在路径中的，不能直接写 sell
+    @GetMapping("/cars/{path}")
+    public Map carsSell(@MatrixVariable("low") Integer low,
+                        @MatrixVariable("brand") List<String> brands,
+                        @PathVariable("path") String path){
+        Map<String, Object> map = new HashMap<>();
+        map.put("low",low);
+        map.put("brand", brands);
+        map.put("path", path); //真实的路径：就是 /cars/sell
+        return map;
+    }
+
+    //@MatrixVariable可以获取路径中的矩阵变量，那么也引来了另一个问题
+    //href="/boss/1;age=20/2;age=10" 不同路径中有同名的矩阵变量怎么办
+    //@MatrixVariable 中的pathVar可以解决
+    /*@MatrixVariable源码：
+    *The name of the URI path variable where the matrix variable is located,
+    * if necessary for disambiguation (e.g. a matrix variable with the same
+    * name present in more than one path segment).
+    * String pathVar() default ValueConstants.DEFAULT_NONE;
+    * */
+    @GetMapping("/boss/{bossId}/{empId}")
+    public Map getBoss(@MatrixVariable(value = "age", pathVar = "bossId") Integer bossAge,
+                       @MatrixVariable(value = "age", pathVar = "empId") Integer empAge){
+        Map<String, Object> map = new HashMap<>();
+        map.put("bossAge", bossAge);
+        map.put("empAge", empAge);
+        return map;
+    }
+
+
+
 }
