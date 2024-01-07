e5537d590da6995c79613b7f4e3613aef501f456
root <1370357561@qq.com>
2024-01-07 23:41:30 +0800

    title: Model,Map原理学习

--- src/main/java/com/zzq/boot/controller/RequestAttrController.java
+++ src/main/java/com/zzq/boot/controller/RequestAttrController.java
@@ -1,12 +1,15 @@
 package com.zzq.boot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +23,18 @@ public class RequestAttrController {
         return "forward:/success"; //转发到 /success请求
     }
 
+    @GetMapping("/params")
+    public String testParam(Map<String,Object> map,
+                            Model model,
+                            HttpServletRequest request,
+                            HttpServletResponse response) {
+        map.put("mapParams", "World map");
+        model.addAttribute("modelParams", "model Param");
+        request.setAttribute("requestParams", "requestMessage");
+        response.addCookie(new Cookie("responseParams","responseCookie"));
+        return "forward:/success";
+    }
+
     /**
      * 请求转发是同一次请求
      * @param msg
@@ -29,13 +44,20 @@ public class RequestAttrController {
      */
     @ResponseBody
     @GetMapping("/success")
-    public Map success(@RequestAttribute("msg") String msg,
-                          @RequestAttribute("code") Integer code,
+    public Map success(@RequestAttribute(value = "msg" , required = false) String msg,
+                          @RequestAttribute(value = "code", required = false) Integer code,
                           HttpServletRequest request) {
         Object msg1 = request.getAttribute("msg");
         Map<String ,Object> map = new HashMap<>();
         map.put("requestMethod_msg", msg1);
         map.put("annotation_msg", msg);
+
+        // 保存 /params 请求中的参数的值
+        map.put("params.map", request.getAttribute("mapParams"));
+        map.put("params.model", request.getAttribute("modelParams"));
+        map.put("params.request", request.getAttribute("requestParams"));
+
+
         return map;
     }
 }
